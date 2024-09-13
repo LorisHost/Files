83a7b2181eed49061eaef00a18610bb970400d0d
LorisHost <130068806+LorisHost@users.noreply.github.com>
2024-09-13 20:31:02 +0300

    Create sortexpc.lua

---
+++ sortexpc.lua
@@ -0,0 +1,4 @@
+---open source
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+local Window = OrionLib:MakeWindow({Name = "Nexus Universal (PC)", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
