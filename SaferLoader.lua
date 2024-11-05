81ba6a7e3a366137d4dd064a4766a3c59f78eaf3
LorisHost <130068806+LorisHost@users.noreply.github.com>
2024-11-05 17:04:48 +0200

    Update SaferLoader.lua

--- SaferLoader.lua
+++ SaferLoader.lua
@@ -1,6 +1,6 @@
 local t = os.clock()
 task.spawn(function()
-  local ok, _ = pcall(game.HttpGetAsync, game, "https://raw.githubusercontent.com/LorisHost/lzosiexw/refs/heads/main/frtxhub.lua")
+  local ok, result = pcall(game.HttpGetAsync, game, "https://raw.githubusercontent.com/LorisHost/lzosiexw/refs/heads/main/frtxhub.lua")
   if ok then
     game:GetService("StarterGui"):SetCore("SendNotification", {
       Title = "Script Loaded",
@@ -8,6 +8,6 @@ task.spawn(function()
       Duration = 5
     })
   else
-    warn("Error loading script.")
+    warn("Error loading script:", result) -- Include the error message for debugging
   end
 end)
