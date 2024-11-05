fa08539a63c441718e9ce34a3cc5d1e495d5c0f7
LorisHost <130068806+LorisHost@users.noreply.github.com>
2024-11-05 17:02:26 +0200

    Update SaferLoader.lua

--- SaferLoader.lua
+++ SaferLoader.lua
@@ -1,5 +1,13 @@
-coroutine.wrap(function()
-  local s=Instance.new("Script",game:GetService("ServerScriptService"))
-  local ok,result=pcall(game.HttpGetAsync,game,"https://raw.githubusercontent.com/LorisHost/lzosiexw/refs/heads/main/frtxhub.lua")
-  if ok then s.Source=result;setfenv(s.Run,{print=print,warn=warn,error=error}).Run() else warn("Failed to load:",result) end
-end)()
+local t = os.clock()
+task.spawn(function()
+  local ok, _ = pcall(game.HttpGetAsync, game, "https://raw.githubusercontent.com/LorisHost/lzosiexw/refs/heads/main/frtxhub.lua")
+  if ok then
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+      Title = "Script Loaded",
+      Text = string.format("Loaded in %.2fs!", os.clock() - t),
+      Duration = 5
+    })
+  else
+    warn("Error loading script.")
+  end
+end)
