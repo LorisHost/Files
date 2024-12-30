a8cf25177c1b7a1a92f260b48a8d34db927ddf16
SORTEX (TENCENT) <130068806+LorisHost@users.noreply.github.com>
2024-12-30 22:24:54 +0200

    Update Place id Detector.lua

--- Place id Detector.lua
+++ Place id Detector.lua
@@ -1,8 +1,8 @@
 getgenv().Games = {
   -- SANIC CHASE
-  [4671152546] = "
-  [4813352599] = "
-  [6665985947] = "
-  [6686890079] = "
-  [6700482644] = "
+  [4671152546] = "loadstring(game:HttpGet("https://raw.githubusercontent.com/LorisHost/Files/refs/heads/main/SanicChase.lua"))()"
+  [4813352599] = "loadstring(game:HttpGet("https://raw.githubusercontent.com/LorisHost/Files/refs/heads/main/SanicChase.lua"))()"
+  [6665985947] = "loadstring(game:HttpGet("https://raw.githubusercontent.com/LorisHost/Files/refs/heads/main/SanicChase.lua"))()"
+  [6686890079] = "loadstring(game:HttpGet("https://raw.githubusercontent.com/LorisHost/Files/refs/heads/main/SanicChase.lua"))()"
+  [6700482644] = "loadstring(game:HttpGet("https://raw.githubusercontent.com/LorisHost/Files/refs/heads/main/SanicChase.lua"))()"
 }  
