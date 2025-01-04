76a181d43ac468f5cbcb72643be3c6db3223e2b6
SORTEX (TENCENT) <130068806+LorisHost@users.noreply.github.com>
2025-01-04 16:28:20 +0200

    Update Elita Ware.lua

--- Elita Ware.lua
+++ Elita Ware.lua
@@ -1,6 +1,6 @@
 local placeScripts = {
     [16896041188] = loadstring(game:HttpGet("https://raw.githubusercontent.com/LorisHost/Files/refs/heads/main/Survive%20nightmare%20XChristianXsj.lua")),
-    [14722542056] = loadstring(game:HttpGet("scriptURL2")),
+    [14722542056] = loadstring(game:HttpGet("https://raw.githubusercontent.com/LorisHost/Files/refs/heads/main/Sonic.EXE%3A%20The%20Disaster%201.3.lua")),
 }
 if placeScripts[game.PlaceId]
     then
