55015d0ac5cd41139aa6850128ddb261faaa8cfc
SORTEX (TENCENT) <130068806+LorisHost@users.noreply.github.com>
2025-01-06 21:58:31 +0200

    Update Elita Ware.lua

--- Elita Ware.lua
+++ Elita Ware.lua
@@ -1,3 +1,11 @@
+local PromptInterface = loadstring(game:HttpGet("https://raw.githubusercontent.com/CookieCrumble2/Prompt-UI/refs/heads/main/load.lua"))()
+PromptInterface.create(
+    "Moonsec V3 Obsfucator is down Update Will be until will be Online", -- Title
+    "bugging functions", -- Description
+    "Accept", -- Primary Button Text
+    "Cancel", -- Secondary Button Text
+    function(response)
+        if response then
 local placeScripts = {
     [16896041188] = loadstring(game:HttpGet("https://raw.githubusercontent.com/LorisHost/Files/refs/heads/main/Survive%20nightmare%20XChristianXsj.lua")),
     [14722542056] = loadstring(game:HttpGet("https://raw.githubusercontent.com/LorisHost/Files/refs/heads/main/Sonic.EXE%3A%20The%20Disaster%201.3.lua")),
@@ -6,3 +14,9 @@ if placeScripts[game.PlaceId]
     then
     placeScripts[game.PlaceId]()
 end
+
+        else
+    print("wait then or join discord server https://discord.gg/EuaYmUxayz")     
+end
+    end
+)
