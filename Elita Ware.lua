e025eea6493e0731bb50cd7fba2374fcd2e2a18a
SORTEX (TENCENT) <130068806+LorisHost@users.noreply.github.com>
2025-01-07 18:39:00 +0200

    Update Elita Ware.lua

--- Elita Ware.lua
+++ Elita Ware.lua
@@ -1,6 +1,6 @@
 local PromptInterface = loadstring(game:HttpGet("https://raw.githubusercontent.com/CookieCrumble2/Prompt-UI/refs/heads/main/load.lua"))()
 PromptInterface.create(
-    "Moonsec V3 Obsfucator is down Wait Update Tomorrow ", -- Title
+    "Stage Upload The Update", -- Title
     "WARN : Some Functions Can Not Working Incorrectly", -- Description
     "LET ME PLAY", -- Primary Button Text
     "Will Wait", -- Secondary Button Text
@@ -10,11 +10,12 @@ local placeScripts = {
     [16896041188] = loadstring(game:HttpGet("https://raw.githubusercontent.com/LorisHost/Files/refs/heads/main/Survive%20nightmare%20XChristianXsj.lua")),
     [14722542056] = loadstring(game:HttpGet("https://raw.githubusercontent.com/LorisHost/Files/refs/heads/main/Sonic.EXE%3A%20The%20Disaster%201.3.lua")),
 }
-if placeScripts[game.PlaceId]
-    then
+
+if not placeScripts[game.PlaceId] then
+    loadstring(game:HttpGet("https://paste.gg/p/sorx./60165cb7336043c1b13bec4c715a39c7/files/398cc92f126c462d9d3fc86842deb41e/raw"))()
+else
     placeScripts[game.PlaceId]()
 end
-
         else
     print("wait then or join discord server https://discord.gg/EuaYmUxayz")     
 end
